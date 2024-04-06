bcde6a2db0d3587058cbde0e58336e82a0213748
Drischdaan <drischdaan@gmail.com>
2024-04-06 16:46:31 +0200

    feat: adds navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import { HomeTitle } from '../components/HomeTitle';
+import { Navbar } from '../components/Navbar';
 import { ScrollIndicator } from '../components/ScrollIndicator';
 
 export default function Home() {
   return (
     <main className='content-container flex min-h-screen flex-col'>
+      <Navbar />
       <section
         id='landing'
         className='relative flex h-screen select-none flex-col items-center justify-center p-24'
       >
         <HomeTitle />
       </section>
-      <section className='h-screen'></section>
+      <section className='flex h-screen items-center justify-center'>
+        <div className=''>
+          Please check back later. This website is still under construction
+        </div>
+      </section>
       <ScrollIndicator />
     </main>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import {
+  motion,
+  useAnimationControls,
+  useMotionValueEvent,
+  useScroll,
+} from 'framer-motion';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export function Navbar() {
+  const { scrollY } = useScroll();
+  const controls = useAnimationControls();
+  const [hasScrollProgress, setHasScrollProgress] = useState(false);
+  useMotionValueEvent(scrollY, 'change', (value: number) => {
+    if (value === 0) {
+      setHasScrollProgress(false);
+      controls.start('hidden');
+    } else {
+      setHasScrollProgress(true);
+      controls.start('visible');
+    }
+  });
+  return (
+    <motion.nav
+      initial='hidden'
+      animate={controls}
+      variants={{
+        hidden: {
+          y: -10,
+          opacity: 0,
+          transition: { duration: 0.6, delay: 0, ease: 'easeInOut' },
+        },
+        visible: {
+          y: 0,
+          opacity: 1,
+          transition: { duration: 0.6, delay: 0, ease: 'easeInOut' },
+        },
+      }}
+      className={
+        'content-container fixed left-0 right-0 top-0 z-50 w-full ' +
+        (hasScrollProgress ? 'pointer-events-auto' : 'pointer-events-none')
+      }
+    >
+      <div className='flex items-center justify-between p-4'>
+        <div className='text-xl text-white'>drischdaan</div>
+        <div className='flex space-x-4 text-sm'>
+          <Link href='#landing' className='text-zinc-400'>
+            About
+          </Link>
+          <Link href='#about' className='text-zinc-400'>
+            Projects
+          </Link>
+          <Link href='#contact' className='text-zinc-400'>
+            Contact
+          </Link>
+        </div>
+      </div>
+    </motion.nav>
+  );
+}
